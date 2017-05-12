6d5752ff00f16c86cbbc07721a7b38f01c7c8e95
Ricardo Quintero <rquintero@itculiacan.edu.mx>
2017-05-11 22:58:45 -0600

    Agregando BeverageWithHook ...

--- coffeeBeverage/src/appBarista/BeverageTestDrive.java
+++ coffeeBeverage/src/appBarista/BeverageTestDrive.java
@@ -13,5 +13,15 @@ public class BeverageTestDrive {
  
 		System.out.println("\nMaking coffee...");
 		coffee.prepareRecipe();
+
+ 
+		TeaWithHook teaHook = new TeaWithHook();
+		CoffeeWithHook coffeeHook = new CoffeeWithHook();
+ 
+		System.out.println("\nMaking tea...");
+		teaHook.prepareRecipe();
+ 
+		System.out.println("\nMaking coffee...");
+		coffeeHook.prepareRecipe();
 	}
 }
